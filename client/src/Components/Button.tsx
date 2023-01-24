09a224d729a68355f4e56c6efc6b1a36e26a60cc
Romulo91 <romulo.galiao@googlemail.com>
2023-01-24 23:32:10 +0100

    test

--- client/src/Components/Button.tsx
+++ client/src/Components/Button.tsx
@@ -7,7 +7,7 @@ export const Button = forwardRef<
   return (
     <button
       className={`border-2 border-gray-900 bg-blue-600 rounded p-2 w-full text-white font-bold hover:bg-blue-500 focus:bg-blue-400
-      transition-colors disabled:bg-gray-500 ${className}`}
+      transition-colors disabled:bg-gray-600 ${className}`}
       ref={ref}
     >
       {children}
